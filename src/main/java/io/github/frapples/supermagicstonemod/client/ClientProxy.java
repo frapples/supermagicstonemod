8696ba512892ff24ea541f54bae4ec24a864b9fc
Frapples <isfrapples@outlook.com>
2017-02-25 11:05:43 +0800

    添加super_stone的材质

--- src/main/java/io/github/frapples/supermagicstonemod/client/ClientProxy.java
+++ src/main/java/io/github/frapples/supermagicstonemod/client/ClientProxy.java
@@ -1,9 +1,17 @@
 package io.github.frapples.supermagicstonemod.client;
 
+import io.github.frapples.supermagicstonemod.SuperThings.SuperStone;
 import io.github.frapples.supermagicstonemod.common.CommonProxy;
+import io.github.frapples.supermagicstonemod.mcutils.Utils;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameData;
+
+import javax.rmi.CORBA.Util;
 
 /**
  * Created by minecraft on 17-2-22.
@@ -13,7 +21,10 @@ public class ClientProxy extends CommonProxy{
     public void preInit(FMLPreInitializationEvent event)
     {
         super.preInit(event);
+
+        registerRender(SuperStone.self(), 0);
     }
+
 
     @Override
     public void init(FMLInitializationEvent event)
@@ -25,5 +36,11 @@ public class ClientProxy extends CommonProxy{
     public void postInit(FMLPostInitializationEvent event)
     {
         super.postInit(event);
+    }
+
+
+    static public void registerRender(Item item, int metadata) {
+        String name = GameData.getItemRegistry().getNameForObject(item).toString();
+        ModelLoader.setCustomModelResourceLocation(item, metadata, new ModelResourceLocation(name, "inventory"));
     }
 }
